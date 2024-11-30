90461c5a7c5207caa859a5c3c7e9a6874216622c
Carti-drillman <mmbk88221@gmail.com>
2024-11-30 21:23:04 +0630

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript code for basic interactivity and functionality
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Welcome to JPHL's website!");
+    // Additional interactivity can be added here as needed
+});
